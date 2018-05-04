720a2f45fa6012b0635df021068653825567f7ae
Dave <Dave.Troupe@gmail.com>
2018-05-03 23:41:12 -0400

    Updated viewController titles

--- ToDo List/Controllers/TodoListViewController.swift
+++ ToDo List/Controllers/TodoListViewController.swift
@@ -20,6 +20,7 @@ class TodoListViewController: UITableViewController {
     var selectedCategory: Category? {
         didSet {
             loadItems()
+            self.title = selectedCategory?.name
         }
     }
     
